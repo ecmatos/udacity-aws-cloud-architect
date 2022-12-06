4e624fdf3bc4b1d359b1743a55e99ec43d4e18ad
Esron Matos <esronmatos@gmail.com>
2022-12-06 10:25:38 -0300

    Remove temporary credentials

--- performance-scalability/Exercise_1/main.tf
+++ performance-scalability/Exercise_1/main.tf
@@ -10,9 +10,9 @@ terraform {
 # TODO: Designate a cloud provider, region, and credentials
 provider "aws" {
   region  = "us-east-1"
-  access_key = "ASIA2OSNI6JLXKV54IWQ"
-  secret_key = "yZu0tajFf0vdlzOq2vrDa4hT4yq1jc1bZ+MuLHFC"
-  token = "FwoGZXIvYXdzENf//////////wEaDJk15yQJCEya8IPu6CLVAQCzdcHliTVIvib3Fd+o8ANiprZq66iR7c2Y76q/K/eKe0veE2reu+/IgChTkSdYUCloelKaPt3sklHaSl3TJCESUBckXJizxv5Rdcb34BbIBpgeauHount95fh5FAcAveJmzsqtEL2H0Ogwf8Q+uEifr4BNasoEus2dy63H4pCz6BTdLm5vPco4IEFvpmobvedPrnFfdQh18C3AG6HiPeNlnLqmzsL41d1392esYJhNiRDDZCFIOrOURxV5H6Q27yY407ic2Di5W7rwZRQOekFbAzSEgijym7ScBjIttsahWnPA6tcmm2ZkAODjlrOywiufnjAnWOwBWF8Kuep8IZf1wLjy7X15Kt5p"
+  access_key = ""
+  secret_key = ""
+  token = ""
 }
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
